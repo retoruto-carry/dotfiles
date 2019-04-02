54373e561300342a5666af062958731d4b3dfebc
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-04-03 06:52:51 +0900

    add: tmux

--- setup.sh
+++ setup.sh
@@ -6,6 +6,13 @@ do
   ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
+# tmux
+DOT_FILES=(.tmux.conf .tmux.conf.local)
+for file in ${DOT_FILES[@]}
+do
+  ln -s $HOME/dotfiles/tmux/$file $HOME/$file
+done
+
 # nvim
 DOT_FILES=(init.vim local_bundles.vim local_init.vim)
 for file in ${DOT_FILES[@]}
