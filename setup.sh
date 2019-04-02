ae1771e82f950bd1e22fb8964d2e59283b52c6d2
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2019-04-03 06:57:06 +0900

    remove: vim

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-DOT_FILES=(.zshrc .vimrc .vimrc.local .vimrc.local.bundles)
-for file in ${DOT_FILES[@]}
-do
-  ln -s $HOME/dotfiles/$file $HOME/$file
-done
+# zsh
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 
 # tmux
 DOT_FILES=(.tmux.conf .tmux.conf.local)
